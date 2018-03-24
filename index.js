364558776593e3ec960d80478e7154b342eeaaea
Abid Hasan <abidhasan@gmail.com>
2018-03-24 17:42:37 -0600

    Multiplier and basic element parsing logic

--- index.js
+++ index.js
@@ -27,6 +27,12 @@ function chemicalFormula(formula) {
     // find innermost MATCHING brackets. Exit loop if no matching brackets exist
     var found = formula.match(/[{][A-Za-z0-9]+[}]|[[][A-Za-z0-9]+[\]]|[(][A-Za-z0-9]+[)]/i);
     if (found === null) break;
+    // find the multiplier after formula. If not given, assume it's 1
+    var multiplier = formula.slice(found.index + found[0].length).match(/^\d+/) || {'0': 1, length: 0};
+    // get a list of all elements ["C12", "H12", "O6"] in current pair of matching brackets
+    var allElements = found[0].match(/([A-Z][a-z]?)(\d*)/g);
+    // if no elements matched, then bracket is invalid (e.g. "{5}" is invalid)
+    if (allElements == null) throw new Error('Subscript found before element(s)');
 
   }
 }
