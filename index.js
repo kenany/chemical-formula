e3042a4ae1f059c3ab918084e1fe180084a97a97
Abid Hasan <abidhasan@gmail.com>
2018-03-24 17:40:56 -0600

    formula validation

--- index.js
+++ index.js
@@ -9,111 +9,15 @@ function getAtomicNumber(symbol) {
 }
 
 function chemicalFormula(formula) {
-  var ret = {};
-  var stack;
-  var molecule = '';
-  var withinParenthesis = false;
+  // This function takes a formula eg C6H12[C2H4]2O5 and converts it to dictionary
+  // of individual elements.
 
-  for (var i = 0, length = formula.length; i < length;) {
-    if (formula.charAt(i) === '(') {
-      withinParenthesis = true;
-      stack = null;
-      i++;
-      continue;
-    }
-    else if (formula.charAt(i) === ')') {
-      withinParenthesis = false;
-      i++;
-      continue;
-    }
-
-    var lengthOfSymbol;
-
-    // First assume two-character element symbol
-    var atomicNumber = getAtomicNumber(formula.substring(i, i + 2));
-
-    // Element's symbol is a single character
-    if (atomicNumber === -1) {
-      atomicNumber = getAtomicNumber(formula.charAt(i));
-      lengthOfSymbol = 1;
-    }
-    else {
-      lengthOfSymbol = 2;
-    }
-
-    var mol;
-
-    // Valid symbol
-    if (atomicNumber > -1) {
-      if (i > 0 && formula.charAt(i - 1) === ')') {
-        mol = chemicalFormula(molecule);
-        forOwn(mol, function(count, key) {
-          if (ret[key]) {
-            ret[key] += count;
-          }
-          else {
-            ret[key] = count;
-          }
-        });
-        molecule = '';
-      }
-
-      stack = symbols[atomicNumber - 1];
-      if (!withinParenthesis) {
-        if (ret[stack]) {
-          ret[stack]++;
-        }
-        else {
-          ret[stack] = 1;
-        }
-      }
-      else {
-        molecule += stack;
-      }
-    }
-    else {
-      var subscript = strictParseInt(formula.substring(i, i + 2));
-      if (isFinite(subscript)) {
-        if (!stack) {
-          throw new Error('Subscript found before element(s)');
-        }
-
-        if (!withinParenthesis && molecule !== '') {
-          mol = chemicalFormula(molecule);
-          forOwn(mol, function(count, key) {
-            if (ret[key]) {
-              ret[key] += count * subscript;
-            }
-            else {
-              ret[key] = count * subscript;
-            }
-          });
-          molecule = '';
-        }
-        else {
-          ret[stack] += subscript - 1;
-          lengthOfSymbol++;
-        }
-      }
-      else {
-        subscript = strictParseInt(formula.charAt(i));
-        if (isFinite(subscript)) {
-          if (!stack) {
-            throw new Error('Subscript found before element(s)');
-          }
-
-          ret[stack] += subscript - 1;
-        }
-        else {
-          ret[stack]++;
-        }
-      }
-    }
-
-    i += lengthOfSymbol;
+  // Basic validation of formula (check for non alphanumeric+bracket characters)
+  if (formula.match(/[^A-Za-z0-9{}[\]()]/g) || formula.match(/[a-z]{2,}/) || formula.match(/^\d/)) {
+    throw new Error('Subscript found before element(s)');
   }
+  return formula;
 
-  return ret;
 }
 
 module.exports = chemicalFormula;
