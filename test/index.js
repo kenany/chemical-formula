7553b7e33f62f17fb200f98fb1810ab09dc4b5a8
Kenan Yildirim <kenan@kenany.me>
2019-06-15 08:57:51 -0400

    deps: eslint@^5.16.0, eslint-config@^6.0.0
    Closes https://github.com/KenanY/chemical-formula/pull/33
    Closes https://github.com/KenanY/chemical-formula/pull/38

--- test/index.js
+++ test/index.js
@@ -10,16 +10,16 @@ test('exports a function', function(t) {
 
 test('common organic compounds', function(t) {
   var COMPOUNDS = [
-    ['C19H29COOH', {C: 20, H: 30, O: 2}, 'abietic acid'],
-    ['C12H10', {C: 12, H: 10}, 'acenaphthene'],
-    ['C12H6O2', {C: 12, H: 6, O: 2}, 'acenaphthoquinone'],
-    ['C6H5Br', {C: 6, H: 5, Br: 1}, 'bromobenzene'],
-    ['C3H4OH(COOH)3', {C: 6, H: 8, O: 7}, 'citric acid'],
-    ['HOCH2CH2OH', {H: 6, O: 2, C: 2}, 'ethylene glycol'],
-    ['C5H11NO2', {C: 5, H: 11, N: 1, O: 2}, 'ethylene glycol'],
-    ['CH3CH(CH3)CH3', {C: 4, H: 10}, '2-methylpropene'],
-    ['NH2CH(C4H5N2)COOH', {N: 3, H: 9, C: 6, O: 2}, 'histidine'],
-    ['H2O', {H: 2, O: 1}, 'water']
+    ['C19H29COOH', { C: 20, H: 30, O: 2 }, 'abietic acid'],
+    ['C12H10', { C: 12, H: 10 }, 'acenaphthene'],
+    ['C12H6O2', { C: 12, H: 6, O: 2 }, 'acenaphthoquinone'],
+    ['C6H5Br', { C: 6, H: 5, Br: 1 }, 'bromobenzene'],
+    ['C3H4OH(COOH)3', { C: 6, H: 8, O: 7 }, 'citric acid'],
+    ['HOCH2CH2OH', { H: 6, O: 2, C: 2 }, 'ethylene glycol'],
+    ['C5H11NO2', { C: 5, H: 11, N: 1, O: 2 }, 'ethylene glycol'],
+    ['CH3CH(CH3)CH3', { C: 4, H: 10 }, '2-methylpropene'],
+    ['NH2CH(C4H5N2)COOH', { N: 3, H: 9, C: 6, O: 2 }, 'histidine'],
+    ['H2O', { H: 2, O: 1 }, 'water']
   ];
 
   t.plan(COMPOUNDS.length);
